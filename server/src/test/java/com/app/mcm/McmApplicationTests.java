e73d112dfd88c656c902c5b55080c86918559bb9
Anton Abitharshan <aptharshan@gmail.com>
2026-02-26 18:01:58 +0530

    chore: initialize client (React) and server (Spring Boot) projects

---
+++ server/src/test/java/com/app/mcm/McmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
